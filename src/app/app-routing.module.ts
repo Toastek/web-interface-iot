4c1c6f1d37a14bd2fad3d3e30c35b03aa94183a2
Toastek <giacomo.kersevan@epitech.eu>
2020-01-23 17:20:04 +0100

    PUSH BEFORE BRANCHING

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { Page2Component } from './pages/page2/page2.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { AlertPageComponent } from './pages/alert-page/alert-page.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'page2', component: Page2Component },
+  { path: 'alert-page', component: AlertPageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './pages/home/home.component';
 import { Page2Component } from './pages/page2/page2.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { AlertPageComponent } from './pages/alert-page/alert-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     Page2Component,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    AlertPageComponent
   ],
   imports: [
     BrowserModule,
